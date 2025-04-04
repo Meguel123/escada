874d3230c967f4644d0a7be8f5d5d38ab8c91bf4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 19:49:41 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+double anguloG, AnguloR, CE, a;
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine("------Escada------\n");
+Console.WriteLine("comprimento da escada é");
+CE = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("ANGULO EM GRAUS da posição é\n");
+anguloG = Convert.ToDouble(Console.ReadLine()!);
+
+AnguloR = Math.PI * anguloG / 180;
+a = CE * Math.Sin(AnguloR);
+
+Console.WriteLine($@"uma escada de {CE} e {anguloG} alcança {a:N2}");
